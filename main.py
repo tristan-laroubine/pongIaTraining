053426a3cfbc3539b8d80a60306fc6c7bf929f25
tristan-laroubine <laroubine.tristan@yahoo.fr>
2021-04-19 00:19:26 +0200

    Adding comment clean up

--- main.py
+++ main.py
@@ -7,54 +7,23 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import utils
 
-def main():
-    env = gym.make('Pong-v4')
-    env.reset()
-    play(env, zoom=3, fps=12, callback=mycallback)
-    env.close()
-
+# variable used to store the data of a hand played game
 XarrayFrame = []
 YarrayFrame = []
 RarrayFrame = []
 
 
-def vectoriseGameOutput(obs_t):
-    return obs_t[34:194:4, 12:148:2, 1]
-
-
-def testIA():
+def records_game_played_by_hand():  # saves the history of a game
     env = gym.make('Pong-v4')
     env.reset()
-    model = get_agent()
-    obs_t, obs_tp1, action, rew, done, info = env.step(env.action_space.sample())  # take a random action
-    while done is not True:
-        x_data = vectoriseGameOutput(obs_t)
-        obs_t, obs_tp1, action, rew, done, info = env.step(model.predict(x_data))
-        if (rew != 0):
-            print(rew)
-
+    play(env, zoom=3, fps=12, callback=saveFrameGame)
     env.close()
 
-def get_agent():
-    x_train = np.asarray(pickle.load(open("X.p", "rb")))
-    y_train = np.asarray(pickle.load(open("Y.p", "rb")))
 
-    model = keras.Sequential()
-    model.add(layers.Dense(16, input_dim=1, activation='relu'))
-    model.add(layers.Dense(8, activation='relu'))
-    model.add(layers.Dense(3))
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-    model.fit(x_train, y_train, epochs=100, validation_split=0.33)
-    #ValueError: Input 0 of layer sequential is incompatible with the layer: expected axis -1 of input shape to have value 1 but received input with shape (None, 2720)
-
-    return model
-
-
-
-def mycallback(obs_t, obs_tp1, action, rew, done, info):
+def saveFrameGame(obs_t, obs_tp1, action, rew, done, info):
     print("action = ", action, " reward = ", rew, "done = ", done)
     # imageio.imwrite("jeu.jpg", obs_t[34:194:4, 12:148:2, 1])
-    matrix_fram = obs_t[34:194:4, 12:148:2, 1]
+    matrix_fram = vectorization_game_output(obs_t)
 
     XarrayFrame.append(np.array(matrix_fram).flatten())
     YarrayFrame.append(action)
@@ -66,8 +35,40 @@ def mycallback(obs_t, obs_tp1, action, rew, done, info):
         exit(0)
 
 
+def vectorization_game_output(obs_t):
+    return obs_t[34:194:4, 12:148:2, 1]
 
+
+def test_IA():
+    env = gym.make('Pong-v4')
+    env.reset()
+    model = get_agent()
+    obs_t, obs_tp1, action, rew, done, info = env.step(env.action_space.sample())  # take a random action
+    while done is not True:
+        x_data = vectorization_game_output(obs_t)
+        obs_t, obs_tp1, action, rew, done, info = env.step(model.predict(x_data))
+        if (rew != 0):
+            print(rew)
+    env.close()
+
+
+def get_agent():
+    x_train = np.asarray(pickle.load(open("X.p", "rb")))
+    y_train = np.asarray(pickle.load(open("Y.p", "rb")))
+
+    model = keras.Sequential()
+    model.add(layers.Dense(16, input_dim=1, activation='relu'))
+    model.add(layers.Dense(8, activation='relu'))
+    model.add(layers.Dense(3))
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+    model.fit(x_train, y_train, epochs=100, validation_split=0.33)
+    # ValueError: Input 0 of layer sequential is incompatible with the layer: expected axis -1 of input shape to have value 1 but received input with shape (None, 2720)
+    return model
 
 
 if __name__ == '__main__':
-    testIA()
+    needCreateRecord = False
+    if needCreateRecord:
+        records_game_played_by_hand()
+    else:
+        test_IA()
