fb6ef5301ceae8e10cb458ed12ac429e516bd9d3
tristan-laroubine <laroubine.tristan@yahoo.fr>
2021-04-19 17:36:59 +0200

    modif + fixe

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # Project created by Lucie MORANT & Tristan LAROUBINE
+
 import gym
 import imageio as imageio
 import numpy as np
 import pickle
+import pygame
+import keras
 from gym.utils.play import play
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -25,7 +28,6 @@ def saveFrameGame(obs_t, obs_tp1, action, rew, done, info):
     print("action = ", action, " reward = ", rew, "done = ", done)
     # imageio.imwrite("jeu.jpg", obs_t[34:194:4, 12:148:2, 1])
     matrix_fram = vectorization_game_output(obs_t)
-
     XarrayFrame.append(np.array(matrix_fram).flatten())
     YarrayFrame.append(action)
     RarrayFrame.append(rew)
@@ -44,10 +46,10 @@ def test_IA():
     env = gym.make('Pong-v4')
     env.reset()
     model = get_agent()
-    obs_t, obs_tp1, action, rew, done, info = env.step(env.action_space.sample())  # take a random action
+    obs_t, rew, done, inf = env.step(env.action_space.sample())  # take a random action
     while done is not True:
         x_data = vectorization_game_output(obs_t)
-        obs_t, obs_tp1, action, rew, done, info = env.step(model.predict(x_data))
+        obs_t, rew, done, inf = env.step(model.predict(x_data))
         if (rew != 0):
             print(rew)
     env.close()
@@ -56,20 +58,18 @@ def test_IA():
 def get_agent():
     x_train = np.asarray(pickle.load(open("X.p", "rb")))
     y_train = np.asarray(pickle.load(open("Y.p", "rb")))
-
+    y_train = keras.utils.to_categorical(y_train, 0)
     model = keras.Sequential()
-    model.add(layers.Dense(16, input_dim=1, activation='relu'))
+    model.add(layers.Dense(16, input_dim=68 * 40, activation='relu'))
     model.add(layers.Dense(8, activation='relu'))
-    model.add(layers.Dense(3))
+    model.add(layers.Dense(4))
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
     model.fit(x_train, y_train, epochs=100, validation_split=0.33)
-    # ValueError: Input 0 of layer sequential is incompatible with the layer: expected axis -1 of input shape to have value 1 but received input with shape (None, 2720)
     return model
 
 
-if __name__ == '__main__':
-    needCreateRecord = False
-    if needCreateRecord:
-        records_game_played_by_hand()
-    else:
-        test_IA()
+needCreateRecord = False
+if needCreateRecord:
+    records_game_played_by_hand()
+else:
+    test_IA()
